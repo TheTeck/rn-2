b97dcbb908fec612a11749095c52ea0e4be14aa2
John Teckemeyer <johnteckemeyer@gmail.com>
2021-07-24 13:43:17 -0700

    Add GameOverScreen component and add logic in App.js for starting new game

--- App.js
+++ App.js
@@ -4,19 +4,32 @@ import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
 
   const [userNumber, setUserNumber] = useState();
+  const [rounds, setRounds] = useState(0);
+
+  function configureNewGameHandler () {
+    setRounds(0);
+    setUserNumber(null);
+  };
 
   function startGameHandler (selectedNumber) {
     setUserNumber(selectedNumber);
-  }
+  };
+
+  function gameOverHandler (numOfRounds) {
+    setRounds(numOfRounds);
+  };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-  if (userNumber) {
-    content = <GameScreen userChoice={userNumber} />;
+  if (userNumber && rounds <= 0) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
+  } else if (rounds > 0) {
+    content = <GameOverScreen roundsNumber={rounds} userNumber={userNumber} onNewGame={configureNewGameHandler} />;
   }
 
   return (
@@ -25,7 +38,7 @@ export default function App() {
       {content}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   screen: {
